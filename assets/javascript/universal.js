512a6d4f4d7d7857e2f83549305c4baa7d7b6936
UEMxMDE <70921784+UEMxMDE@users.noreply.github.com>
2026-02-04 21:14:47 -0800

    Some changes
    - Updated every page to be linked to universal.js
    - Made the <head> tag of every page more organized
    - Updated footer to allow dynamic updating of the "Website last updated on" date by changing a single hardcoded const in universal.js
    - Moved the <script> link for "1_12_2_wiki.js" in the 1.12.2 wiki page to the <head> tag and added "defer" to it

---
+++ assets/javascript/universal.js
@@ -0,0 +1,9 @@
+// Footer date information
+const SITE_LAST_UPDATED = "February 4, 2026";	// Update this manually before committing changes to github
+const PAGE_LAST_UPDATED = document.body.dataset.pageUpdated;
+document.addEventListener("DOMContentLoaded", () => {
+	const el = document.getElementById("site-last-updated");
+	if (!el) return;
+	
+	el.textContent = SITE_LAST_UPDATED;
+});
