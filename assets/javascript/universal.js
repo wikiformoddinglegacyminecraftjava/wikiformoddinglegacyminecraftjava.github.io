fe42c6205a902ca07e170a942fe0fa650a5a9607
UEMxMDE <70921784+UEMxMDE@users.noreply.github.com>
2026-02-11 13:02:32 -0800

    Massive update to the 1.12.2 Forge wiki page
    - Small update made to 1.7.10 Special wiki page

--- assets/javascript/universal.js
+++ assets/javascript/universal.js
@@ -1,5 +1,5 @@
 // Footer date information
-const SITE_LAST_UPDATED = "February 8, 2026";	// Update this manually before committing changes to github
+const SITE_LAST_UPDATED = "February 11, 2026";	// Update this manually before committing changes to github
 document.addEventListener("DOMContentLoaded", () => {
 	const el = document.getElementById("site-last-updated");
 	if (!el) return;
